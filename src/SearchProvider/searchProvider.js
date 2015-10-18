90e153e5534c9c96b6abe5d95f93eef732410c4d
Marcel Tiede <badcel@users.noreply.github.com>
2015-10-18 17:59:02 +0200

    Skeleton for search provider service

---
+++ src/SearchProvider/searchProvider.js
@@ -0,0 +1,67 @@
+/*
+ *   Copyright (c) 2013 Giovanni Campagna <scampa.giovanni@gmail.com>
+ *   Copyright (C) 2015  Marcel Tiede
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+const Gio = imports.gi.Gio;
+const GLib = imports.gi.GLib;
+const Lang = imports.lang;
+
+const Util = imports.misc.util;
+
+const SearchProviderInterface = Gio.resources_lookup_data('/org/gnome/shell/ShellSearchProvider2.xml', 0).toArray().toString();
+
+const SearchProvider = new Lang.Class({
+    Name: 'WebMusicSearchProvider',
+
+    _init: function(application) {
+        this._app = application;
+
+        this._impl = Gio.DBusExportedObject.wrapJSObject(SearchProviderInterface, this);
+    },
+
+    export: function(connection, path) {
+        return this._impl.export(connection, path);
+    },
+
+    unexport: function(connection) {
+        return this._impl.unexport_from_connection(connection);
+    },
+
+    GetInitialResultSetAsync: function(params, invocation) {
+        let ret = [];
+
+        return ret;
+    },
+
+    GetSubsearchResultSet: function(previous, terms) {
+        let ret = [];
+
+        return ret;
+    },
+
+    GetResultMetas: function(identifiers) {
+        let ret = [];
+
+        return ret;
+    },
+
+    ActivateResult: function(id, terms, timestamp) {
+    },
+
+    LaunchSearch: function(terms, timestamp) {
+    },
+});
+
---
+++ src/SearchProvider/service.js
@@ -0,0 +1,93 @@
+/*
+ *   Copyright (c) 2012 Giovanni Campagna <scampa.giovanni@gmail.com>
+ *   Copyright (C) 2015  Marcel Tiede
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+pkg.initGettext();
+pkg.initFormat();
+pkg.require({ 'Gio': '2.0',
+              'GLib': '2.0',
+              'GObject': '2.0'});
+
+const Gio = imports.gi.Gio;
+const GLib = imports.gi.GLib;
+const Lang = imports.lang;
+
+const Util = imports.misc.util;
+const SearchProvider = imports.SearchProvider.searchProvider;
+
+function initEnvironment() {
+    window.getApp = function() {
+        return Gio.Application.get_default();
+    };
+}
+
+const Service = new Lang.Class({
+    Name: 'WebMusicService',
+    Extends: Gio.Application,
+
+    _init: function() {
+        this.parent({ application_id: pkg.name,
+                      flags: Gio.ApplicationFlags.IS_SERVICE,
+                      inactivity_timeout: 60000 });
+        GLib.set_application_name(_("WebMusic"));
+
+        this._searchProvider = new SearchProvider.SearchProvider(this);
+    },
+
+    _onQuit: function() {
+        this.quit();
+    },
+
+    vfunc_dbus_register: function(connection, path) {
+        this.parent(connection, path);
+
+        this._searchProvider.export(connection, path);
+        return true;
+    },
+
+/*
+  Can't do until GApplication is fixed.
+
+    vfunc_dbus_unregister: function(connection, path) {
+        this._searchProvider.unexport(connection);
+
+        this.parent(connection, path);
+    },
+*/
+
+    vfunc_startup: function() {
+        this.parent();
+
+        Util.initActions(this,
+                         [{ name: 'quit',
+                            activate: this._onQuit }]);
+    },
+
+    vfunc_activate: function() {
+        // do nothing, this is a background service
+    },
+
+    vfunc_shutdown: function() {
+        this.parent();
+    }
+});
+
+function main(argv) {
+    initEnvironment();
+
+    return (new Service()).run(argv);
+}
+
